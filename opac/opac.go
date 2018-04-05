dde589dcf43f0044beff75d93e7ce3d5a1e8a5c0
ken <block24block@gmail.com>
2018-04-05 22:11:04 +0800

    abide go code style

--- opac/opac.go
+++ opac/opac.go
@@ -8,6 +8,7 @@ import (
 	"github.com/panthesingh/goson"
 )
 
+// Hold is about book storage
 type Hold struct {
 	Callno   string
 	State    string
@@ -15,50 +16,51 @@ type Hold struct {
 	Location string
 }
 
+// Get by book id
 func Get(id string) []Hold {
 
-	hold_list := make([]Hold, 0)
+	holdList := make([]Hold, 0)
 
-	json := get_json(id)
+	json := getJSON(id)
 
 	g, _ := goson.Parse(json)
 
 	holds := g.Get("holdingList")
-	state_map := g.Get("holdStateMap")
-	local_map := g.Get("localMap")
-	lib_map := g.Get("libcodeMap")
+	stateMap := g.Get("holdStateMap")
+	localMap := g.Get("localMap")
+	libMap := g.Get("libcodeMap")
 
 	for i := 0; i < holds.Len(); i++ {
 		hold := holds.Index(i)
 
-		state_code := hold.Get("state").String()
-		state_name := state_map.Get(state_code).Get("stateName").String()
+		stateCode := hold.Get("state").String()
+		stateName := stateMap.Get(stateCode).Get("stateName").String()
 
-		loc_code := hold.Get("curlocal").String()
-		loc_name := local_map.Get(loc_code).String()
+		locCode := hold.Get("curlocal").String()
+		locName := localMap.Get(locCode).String()
 
-		lib_code := hold.Get("curlib").String()
-		lib_name := lib_map.Get(lib_code).String()
+		libCode := hold.Get("curlib").String()
+		libName := libMap.Get(libCode).String()
 
 		h := Hold{
 			Callno:   hold.Get("callno").String(),
-			State:    state_name,
-			Lib:      lib_name,
-			Location: loc_name,
+			State:    stateName,
+			Lib:      libName,
+			Location: locName,
 		}
-		hold_list = append(hold_list, h)
+		holdList = append(holdList, h)
 	}
-	return hold_list
+	return holdList
 }
 
-func get_json(id string) []byte {
+func getJSON(id string) []byte {
 	resp, err := http.Get("http://opac.gzlib.gov.cn/opac/api/holding/" + id)
-
-	defer resp.Body.Close()
 
 	if err != nil {
 		panic(err)
 	}
+
+	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 
