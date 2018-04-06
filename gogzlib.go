80b7f51b700760fc9422a8c997b24a258d859af3
ken <block24block@gmail.com>
2018-04-06 22:27:32 +0800

    accept keywords through cli

--- gogzlib.go
+++ gogzlib.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/turnon/gogzlib/opac"
 )
 
 func main() {
+
+	flag.Parse()
+	keywords := flag.Args()
+
+	if len(keywords) <= 0 {
+		fmt.Println("no keyword given")
+		return
+	}
+
+	keyword := strings.Join(keywords, "%20")
+
 	start := time.Now()
-	opac.Search("ruby")
+	opac.Search(keyword)
 	fmt.Println(time.Now().Sub(start))
 }
--- opac/search.go
+++ opac/search.go
@@ -8,9 +8,9 @@ import (
 )
 
 const searchURL = "http://opac.gzlib.gov.cn/opac/search?" +
-	"searchType=standard&isFacet=true&view=standard&searchWay=title&" +
-	"rows=10&sortWay=score&sortOrder=desc&hasholding=1&" +
-	"searchWay0=marc&logical0=AND&page=1&q="
+	"searchWay=title&searchSource=reader&booktype=&scWay=dim&" +
+	"marcformat=&sortWay=score&sortOrder=desc&startPubdate=&" +
+	"endPubdate=&rows=10&hasholding=1&curlibcode=GT&q="
 
 // Search from http://opac.gzlib.gov.cn/opac/search
 func Search(keyword string) {
