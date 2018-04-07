4a5708683d8c69f74a0a5e4fc81854e7c32b9d17
ken <block24block@gmail.com>
2018-04-07 20:59:04 +0800

    do not pull from workers and sort, just store in order before push to workers

--- opac/book_list.go
+++ opac/book_list.go
@@ -1,71 +1,44 @@
 package opac
 
 import (
-	"sort"
 	"sync"
 )
 
 type bookList struct {
-	books               []*book
-	workers, result     chan *book
-	workersWg, resultWg sync.WaitGroup
-}
-
-type booksSortByNo []book
-
-func (bs booksSortByNo) Len() int {
-	return len(bs)
-}
-
-func (bs booksSortByNo) Less(i, j int) bool {
-	return bs[i].No < bs[j].No
-}
-
-func (bs booksSortByNo) Swap(i, j int) {
-	bs[i], bs[j] = bs[j], bs[i]
+	books   []*book
+	workers chan *book
+	wg      sync.WaitGroup
 }
 
 func (list *bookList) init(n int) {
 	list.books = make([]*book, 0)
-	list.workers, list.result = make(chan *book), make(chan *book)
-	list.workersWg.Add(n)
-	list.resultWg.Add(1)
+	list.workers = make(chan *book)
+	list.wg.Add(n)
 
 	for ; n > 0; n-- {
 		go func() {
 			for b := range list.workers {
 				b.getBookInfo()
-				list.result <- b
 			}
-			list.workersWg.Done()
+			list.wg.Done()
 		}()
 	}
-
-	go func() {
-		for b := range list.result {
-			list.books = append(list.books, b)
-		}
-		list.resultWg.Done()
-	}()
 }
 
 func (list *bookList) process(b *book) {
+	list.books = append(list.books, b)
 	list.workers <- b
 }
 
 func (list *bookList) done() []book {
 	close(list.workers)
-	list.workersWg.Wait()
-	close(list.result)
-	list.resultWg.Wait()
+	list.wg.Wait()
 
-	books := make(booksSortByNo, 0, len(list.books))
+	books := make([]book, 0, len(list.books))
 
 	for _, b := range list.books {
 		books = append(books, *b)
 	}
-
-	sort.Sort(books)
 
 	return books
 }
